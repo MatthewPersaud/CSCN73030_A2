0223c8ae454e2cc360169bb18b8ea8a495e8cfda
MatthewPersaud <Mpersaud4116@conestogac.on.ca>
2022-09-22 21:41:36 -0400

    STUDENT_DATA struct created and reads from StudentData.txt

--- CSCN73030_A2/Source.cpp
+++ CSCN73030_A2/Source.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
-#include <string.h>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+struct STUDENT_DATA {
+	string firstName;
+	string lastName;
+};
+
 int main() {
 
+	ifstream inputFile("StudentData.txt");
+
+	vector<STUDENT_DATA> students;
+	STUDENT_DATA newStudent;
+
+	string lineData;
+
+	int commaIndex;
+
+	while (getline(inputFile, lineData)) {
+		commaIndex = lineData.find(",");
+
+		newStudent.lastName = lineData.substr(0, commaIndex);
+		newStudent.firstName = lineData.substr(commaIndex + 1, lineData.length());
+
+		students.push_back(newStudent);
+	}
 
 	return 1;
 }
